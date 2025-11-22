eb81baf648e3dc46639eae2f80933766304d8a10
johnderrickos12-pixel <johnderrickos12@gmail.com>
2025-11-22 17:07:05 +0800

    feat: lift selected file state to App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,31 @@
-import React from 'react';
-import Layout from './components/Layout';
+import { useState } from 'react';
+import Header from './components/Header';
+import Sidebar from './components/Sidebar';
+import ChatInterface from './components/ChatInterface';
+import PreviewWindow from './components/PreviewWindow';
+import { FileNode } from './data/mockData';
 
-const App: React.FC = () => {
+export default function App() {
+  const [selectedFile, setSelectedFile] = useState<FileNode | null>(null);
+
+  const handleFileSelect = (file: FileNode) => {
+    setSelectedFile(file);
+  };
+
   return (
-    <div className="bg-gray-900 text-gray-100 min-h-screen font-sans">
-      <Layout />
+    <div className="flex h-screen bg-gray-900 text-white font-sans">
+      <Sidebar onFileSelect={handleFileSelect} selectedFile={selectedFile} />
+      <div className="flex flex-col flex-1">
+        <Header />
+        <main className="flex flex-1 overflow-hidden">
+          <div className="flex-1 flex flex-col overflow-hidden">
+            <ChatInterface />
+          </div>
+          <div className="w-1/2 border-l border-gray-700 flex flex-col">
+            <PreviewWindow selectedFile={selectedFile} />
+          </div>
+        </main>
+      </div>
     </div>
   );
-};
-
-export default App;
+}
