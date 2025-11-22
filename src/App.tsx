1eef7c5a32f1c8632b86069a81eb9efb4358e338
johnderrickos12-pixel <johnderrickos12@gmail.com>
2025-11-22 17:03:02 +0800

    feat: create main App component shell

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from './components/Layout';
+
+const App: React.FC = () => {
+  return (
+    <div className="bg-gray-900 text-gray-100 min-h-screen font-sans">
+      <Layout />
+    </div>
+  );
+};
+
+export default App;
